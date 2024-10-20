536323b437f36d588b82b02e08bc4bfc4236dd2e
Mark Blumenthal <markblumenthal@live.com>
2024-10-20 14:09:58 +0300

    changes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// frontend/src/index.jsx
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'; 
+import { BrowserRouter } from 'react-router-dom';  
+import App from './App.jsx';
+import store from './store';  
+import './index.css';  
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>  {/* Wrap App in Redux Provider */}
+      <BrowserRouter>  {/* Wrap App in React Router */}
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
+
